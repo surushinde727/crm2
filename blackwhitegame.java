a2ac20f38cfe5322e743d57f8787052411748592
surushinde727 <130980556+surushinde727@users.noreply.github.com>
2023-04-17 16:20:37 +0530

    Update blackwhitegame.java

--- blackwhitegame.java
+++ blackwhitegame.java
@@ -7,13 +7,13 @@ class blackwhitegame
 		Scanner sc=new Scanner(System.in);
 		System.out.println("1.b 2.w ");
 
-		System.out.println("enter your choice user1");
+		System.out.println("user1");
 		char x=sc.next().charAt(0);
 
-		System.out.println("enter your choice user2");
+		System.out.println(" user2");
 		char y=sc.next().charAt(0);
                 
-                System.out.println("enter your choice user3");
+                System.out.println(" user3");
 		char z=sc.next().charAt(0);
 
 		if(x==1)
@@ -33,6 +33,54 @@ class blackwhitegame
                         else if(y==1 && z==2)
 			{
 				System.out.println("user3 won match");
+			}
+			else
+			{
+				System.out.println("open your eyes");
+			}
+		}
+		
+		  if(y==1)
+		{
+			if(x==1 && z==1)
+			{
+				System.out.println("match drop");
+			}
+			else if(x==2 && z==2)
+			{
+				System.out.println("user1 won match");
+			}
+			else if(x==2 && z==1)
+			{
+				System.out.println("user1 won match");
+			}
+                        else if(x==1 && z==2)
+			{
+				System.out.println("user3 won match");
+			}
+			else
+			{
+				System.out.println("open your eyes");
+			}
+		}
+
+                if(z==1)
+		{
+			if(x==1 && y==1)
+			{
+				System.out.println("match drop");
+			}
+			else if(x==2 && y==2)
+			{
+				System.out.println("user3 won match");
+			}
+			else if(x==2 && y==1)
+			{
+				System.out.println("user3 won match");
+			}
+                        else if(x==1 && y==2)
+			{
+				System.out.println("user2 won match");
 			}
 			else
 			{
